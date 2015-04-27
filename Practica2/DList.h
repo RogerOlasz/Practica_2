d28a219e61eeacd1346bceb57f2dbea2634945c0
RogerOlasz <rogerolasz96@gmail.com>
2015-04-27 18:29:11 +0200

    Done

---
+++ Practica2/DList.h
@@ -0,0 +1,203 @@
+#ifndef __DList_H__
+#define __DList_H__
+
+#include <iostream>
+
+template<class TYPE>
+struct  DNode
+{
+	TYPE data;
+	DNode<TYPE>* next;
+	DNode<TYPE>* previous;
+};
+
+template<class TYPE>
+class DList {
+
+private:
+	DNode<TYPE>* start;
+
+public:
+	DList<TYPE>()
+	{
+		start = NULL;
+	}
+
+	unsigned int Count() const
+	{
+		unsigned int counter = 0;
+		DNode<TYPE>* tmp = start;
+
+		while (tmp != NULL)
+		{
+			tmp = tmp->next;
+			counter++;
+		}
+
+		return counter;
+	}
+
+	void Add(TYPE new_data)
+	{
+		DNode<TYPE>* new_node = new DNode<TYPE>;
+		new_node->data = new_data;
+		new_node->next = NULL;
+
+		if (start != NULL)
+		{
+			DNode<TYPE>* tmp = start;
+
+			while (tmp->next != NULL)
+			{
+				tmp = tmp->next;
+			}
+				
+			tmp->next = new_node;
+			new_node->previous = tmp;
+		}
+
+		else
+		{
+			new_node->previous = NULL;
+			start = new_node;
+		}
+	}
+
+	bool Del(DNode<TYPE>* delete_node)
+	{
+		if (start != NULL && delete_node != NULL)
+		{
+			if (start != delete_node)
+			{
+				DNode<TYPE>* tmp = start;
+
+				while (tmp->next != delete_node)
+				{
+					tmp = tmp->next;
+					if (tmp->next == NULL)
+					{
+						return false;
+					}	
+				}
+
+				tmp->next = delete_node->next;
+
+				if (delete_node->next != NULL)
+				{
+					delete_node->next->previous = tmp;
+				}
+				else
+				{
+					tmp->next = NULL;
+				}
+			}
+			else
+			{
+				if (delete_node->next != NULL)
+				{
+					start = delete_node->next;
+					delete_node->next->previous = NULL;
+				}
+				else
+				{
+					start = NULL;
+				}
+					
+			}
+			delete delete_node;
+			return true;
+		}
+		return false;
+	}
+
+	bool DelList() 
+	{
+		if (start != NULL)
+		{
+			while (start->next != NULL)
+			{
+				DNode<TYPE>* node_to_delete = start;
+				start = start->next;
+				delete node_to_delete;
+			}
+			start = NULL;
+			return true;
+		}
+		return false;
+	}
+
+	DNode<TYPE> *getFirst() const
+	{
+		return start;
+	}
+
+	DNode<TYPE> *getLast() const
+	{
+		if (start != NULL)
+		{
+			DNode<TYPE>* tmp = start;
+			while (tmp->next != NULL)
+			{
+				tmp = tmp->next;
+			}		
+			return tmp;
+		}
+		return NULL;
+	}
+
+	DNode<TYPE>* getNodeAtPos(unsigned int position) const
+		{
+			if (start != NULL && position < Count())
+			{
+				unsigned int position_counter = 0;
+				DNode<TYPE>* tmp = start;
+
+				while (position_counter != position)
+				{
+					tmp = tmp->next;
+					position_counter++;
+				}
+				return tmp;
+			}
+			return NULL;
+		}
+
+	int Find(const TYPE& data)
+	{
+		DNode<TYPE>* tmp = start;
+		unsigned int index = 0;
+
+		while (tmp != NULL)
+		{
+			if (tmp->data == data)
+			{
+				return (index);
+			}
+
+		++index;
+		tmp = tmp->next;
+		}
+	return (-1);
+	}
+
+	TYPE& operator  [](const unsigned int index)
+	{
+		long pos;
+		DNode<TYPE>* item;
+		pos = 0;
+		item = start;
+
+		while (item != NULL)
+		{
+			if (pos == index)
+			{
+				break;
+			}
+		++pos;
+		item = item->next;
+		}
+	return(item->data);
+	}
+};
+
+#endif //__DList_H__
---
+++ Practica2/Tree.h
@@ -0,0 +1,273 @@
+#ifndef __Tree_H__
+#define __Tree_H__
+
+#include "DList.h"
+#include "Stack.h"
+
+//Class TreeNodes from Tree class
+template<class TreeData>
+class TreeNode
+{
+
+public:
+
+	TreeData data;
+	TreeNode* parent;
+	DList<TreeNode*> children;
+
+public:
+
+	TreeNode(const TreeData& new_data)
+	{
+		data = new_data;
+		parent = NULL;
+	}
+
+	void AddChild(TreeNode* node)
+	{
+		children.Add(node);
+		node->parent = this;
+	}
+
+	void PreOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		list->Add(this);
+
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			item->data->PreOrderRecursive(list);
+		}
+			
+	}
+
+	void InOrderRecursive(DList<TreeNode<TreeData>*>* list) //Leave odd TreeNode on right
+	{
+		DNode<TreeNode*>* item = children.getFirst();
+		unsigned int mid = children.Count() / 2;
+
+		for (unsigned int i = 0; i < mid; ++i, item = item->next)
+		{
+			item->data->InOrderRecursive(list);
+		}
+			
+		list->Add(this);
+
+		for (; item != NULL; item = item->next)
+		{
+			item->data->InOrderRecursive(list);
+		}
+			
+	}
+
+	void PostOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			item->data->PostOrderRecursive(list);
+		}
+			
+		list->Add(this);
+	}
+
+	TreeNode<TreeData>* FindRecursive(const TreeData& node) //To find any TreeNode
+	{
+		if (node == data)
+		{
+			return this;
+		}
+			
+		TreeNode<TreeData>* result = NULL;
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+			result = son->FindRecursive(node);
+
+			if (result != NULL)
+			{
+				break;
+			}	
+		}
+	return result;
+	}
+
+	void CollectAll(DList<TreeNode*>* list) //To collect all TreeNodes
+	{
+		list->Add(this);
+
+		DNode<TreeNode*>* item = children.start;
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+			son->CollectAll(list);
+		}
+	}
+
+	void CollectAllData(DList<TreeData>* list)//To collect all TreeNodes data
+	{
+		list->Add(data);
+
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+			son->CollectAllData(list);
+		}
+	}
+
+};
+
+//Tree class 
+template<class TreeData>
+class Tree
+{
+public:
+
+	TreeNode<TreeData>	root_node;
+
+public:
+
+	Tree(const TreeData& new_data) : root_node(new_data)
+	{}
+
+	~Tree()
+	{}
+
+	//Recursive methods
+	void PreOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		root_node.PreOrderRecursive(list);
+	}
+
+	void PostOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		root_node.PostOrderRecursive(list);
+	}
+
+	void InOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		root_node.InOrderRecursive(list);
+	}
+
+	//Iterative methods
+	void PreOrderIterative(DList<TreeNode<TreeData>*>* list)
+	{
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node != NULL || stack.Pop(node))
+		{
+			list->Add(node);
+
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+			for (; item != node->children.getFirst(); item = item->previous)
+			{
+				stack.Push(item->data);
+			}
+			
+			if (item != NULL)
+			{
+				node = item->data;
+			}
+			else
+			{
+				node = NULL;
+			}
+		}
+	}
+
+	void PostOrderIterative(DList<TreeNode<TreeData>*>* list)
+	{
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node != NULL || stack.Pop(node))
+		{
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+
+			if (item != NULL && list->Find(item->data) == -1)
+			{
+				stack.Push(node);
+
+				for (; item != node->children.getFirst(); item = item->previous)
+				{
+					stack.Push(item->data);
+				}
+
+				node = item->data;
+			}
+			else
+			{
+				list->Add(node);
+				node = NULL;
+			}
+		}
+	}
+
+	void InOrderIterative(DList<TreeNode<TreeData>*>* list)
+	{
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node != NULL || stack.Pop(node))
+		{
+			list->Add(node);
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+
+			for (; item != node->children.getFirst(); item = item->previous)
+			{
+				stack.Push(item->data);
+			}
+
+			if (item != NULL)
+			{
+				node = item->data;
+			}
+			else
+			{
+				node = NULL;
+			}
+		}
+	}
+
+	void Add(const TreeData& data)
+	{
+		TreeNode<TreeData>* new_node = new TreeNode<TreeData>(data);
+		root_node.AddChild(new_node);
+	}
+
+	void Add(const TreeData& data, const TreeData& parent)
+	{
+		TreeNode<TreeData>* dad = root_node.FindRecursive(parent);
+		TreeNode<TreeData>* new_node = new TreeNode<TreeData>(data);
+		dad->AddChild(new_node);
+	}
+
+	void Clear()
+	{
+		DList<TreeNode<TreeData>*> nodes_deleted;
+		root_node.CollectAll(&nodes_deleted);
+
+		DNode<TreeNode<TreeData>*>* item = nodes_deleted.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode<TreeData>* son = item->data;
+
+			if (son->parent)
+			{
+				son->parent->RemoveChild(son);
+			}	
+		}
+	}
+
+};
+
+#endif // __Tree_H__
