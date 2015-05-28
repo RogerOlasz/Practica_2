019ec0a14bf2000a0155d61c5bcab3cb3434f7ca
RogerOlasz <rogerolasz96@gmail.com>
2015-05-28 09:37:59 +0200

    Doine

--- Practica2/Main.cpp
+++ Practica2/Main.cpp
@@ -1,25 +1,4 @@
-#include "DList.h"
-#include <random>
-
 
 int main(int argc, char** argv)
 {
-	DList<int> List1;
-
-	unsigned int count = 0;
-
-	for (unsigned int i = 0; i <= 10000; i++)
-	{
-		List1.Add(rand());
-	}
-
-	printf("%d\n%d\n", count, List1.Count());
-
-	count = List1.BubbleSort();
-
-	printf("%d\n%d", count, List1.Count());
-
-	getchar();
-
-	return 0;
 }
